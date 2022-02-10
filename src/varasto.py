ccbb8782b82f96296b3e73c4d7fb337c19f18490
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-02-10 15:00:05 +0200

    Add pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
+        self.tilavuus = max(tilavuus, 0.0)
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -23,6 +19,7 @@ class Varasto:
     def lisaa_varastoon(self, maara):
         if maara < 0:
             return
+
         if maara <= self.paljonko_mahtuu():
             self.saldo = self.saldo + maara
         else:
@@ -31,7 +28,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +37,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
